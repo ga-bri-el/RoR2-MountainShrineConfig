ac49299f05467675621a899059316989d6056cbd
Gabriel Casado <gdgcasado@gmail.com>
2021-06-25 17:43:23 -0300

    initial commit

---
+++ MountainShrineConfig/MountainShrineConfigPlugin.cs
@@ -0,0 +1,137 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using RoR2;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace MountainShrineConfig
+{
+    [BepInDependency("com.bepis.r2api")]
+    [BepInPlugin(GUID, MODNAME, VERSION)]
+    public sealed class MountainShrineConfigPlugin : BaseUnityPlugin
+    {
+        public const string
+            MODNAME = "MountainShrineConfig",
+            AUTHOR = "owl777",
+            GUID = "com." + AUTHOR + "." + MODNAME,
+            VERSION = "1.0.0";
+
+        static ConfigEntry<int> minBossShrineCount;
+        static ConfigEntry<int> maxBossShrineCount;
+        static SceneInfo currentScene;
+        static int bossShrineCardSelectedCount;
+        static bool addedBossCardToOriginalDeck;
+
+        static readonly string[] validStages = { "golemplains", "blackbeach", "goolake", "foggyswamp", "frozenwall", "wispgraveyard", "dampcavesimple", "shipgraveyard", "skymeadow", "rootjungle" };
+        static readonly string[] bossCardStages = { "golemplains", "blackbeach", "goolake", "foggyswamp", "dampcavesimple", "shipgraveyard", "skymeadow", "rootjungle" };
+        static readonly WeightedSelection<DirectorCard> bossDeck = new WeightedSelection<DirectorCard>(capacity: 1);
+        static readonly Dictionary<string, int> bossShrineWeights = new Dictionary<string, int>() 
+        {
+            { "frozenwall", 2 },
+            { "wispgraveyard", 10 },
+        };
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Code Quality", "IDE0051:Remove unused private members", Justification = "Awake is automatically called by Unity")]
+        private void Awake()
+        {
+            Log.Init(Logger);
+            InitializeConfigEntries();
+            InitializeBossDeck();
+            On.RoR2.SceneDirector.SelectCard += OnSelectCard;
+            On.RoR2.ClassicStageInfo.Awake += OnStageChange;
+        }
+
+        private void InitializeConfigEntries() 
+        {
+            minBossShrineCount = Config.Bind<int>(
+                section: "Quantity",
+                key: "Minimum",
+                defaultValue: 0,
+                description: "Minimum amount of mountain shrines per stage."
+            );
+            maxBossShrineCount = Config.Bind<int>(
+                section: "Quantity",
+                key: "Maximum",
+                defaultValue: 3,
+                description: "Maximum amount of mountain shrines per stage."
+            );
+        }
+
+        private void InitializeBossDeck()
+        {
+            bossDeck.AddChoice(NewBossChoiceInfo());
+        }
+
+        private WeightedSelection<DirectorCard>.ChoiceInfo NewBossChoiceInfo(int selectionWeight = 1)
+        {
+            var bossCard = Resources.Load<InteractableSpawnCard>("spawncards/interactablespawncard/iscShrineBoss");
+            var bossChoiceInfo = new WeightedSelection<DirectorCard>.ChoiceInfo();
+            var bossChoiceDirectorCard = new DirectorCard
+            {
+                spawnCard = bossCard,
+                selectionWeight = selectionWeight,
+                spawnDistance = DirectorCore.MonsterSpawnDistance.Standard,
+                allowAmbushSpawn = true,
+                preventOverhead = false,
+                minimumStageCompletions = 0
+            };
+            bossChoiceInfo.value = bossChoiceDirectorCard;
+            return bossChoiceInfo;
+        }
+        
+        private DirectorCard OnSelectCard(On.RoR2.SceneDirector.orig_SelectCard orig, SceneDirector self, WeightedSelection<DirectorCard> deck, int maxCost)
+        {
+            if (!IsValidStage() || !CanAffordBossShrine(maxCost) || !ShouldSpawnBossShrine())
+            {
+                Log.LogInfo("Using original deck.");
+                if (ShouldRemoveBossCardFromDeck()) RemoveBossCard(deck);
+                else if (ShouldAddBossCardToDeck()) AddBossCard(deck);
+                var selectedCard = orig(self, deck, maxCost);
+                if (selectedCard.spawnCard.name.ToLower().Equals("iscshrineboss")) bossShrineCardSelectedCount++;
+                return selectedCard;
+            }
+            bossShrineCardSelectedCount++;
+            Log.LogInfo($"Using boss deck ({bossShrineCardSelectedCount}/{minBossShrineCount.Value}).");
+            return orig(self, bossDeck, maxCost);
+        } 
+
+        private void OnStageChange(On.RoR2.ClassicStageInfo.orig_Awake orig, ClassicStageInfo self) 
+        {
+            bossShrineCardSelectedCount = 0;
+            addedBossCardToOriginalDeck = false;
+            currentScene = self.GetComponent<SceneInfo>();
+            orig(self);
+        }
+
+        private void AddBossCard(WeightedSelection<DirectorCard> deck)
+        {
+            var selectionWeight = bossShrineWeights[currentScene.sceneDef.baseSceneName];
+            deck.AddChoice(NewBossChoiceInfo(selectionWeight: selectionWeight));
+            addedBossCardToOriginalDeck = true;
+            Log.LogInfo($"Added boss card to original deck with selection weight of {selectionWeight}");
+        }
+
+        private bool ShouldAddBossCardToDeck() => !ShouldRemoveBossCardFromDeck() && !bossCardStages.Contains(currentScene.sceneDef.baseSceneName) && !addedBossCardToOriginalDeck;
+
+        private void RemoveBossCard(WeightedSelection<DirectorCard> deck)
+        {
+            for (var i = 0; i < deck.Count; i++)
+            {
+                if (deck.GetChoice(i).value.spawnCard.name.ToLower().Equals("iscshrineboss"))
+                {
+                    deck.RemoveChoice(i);
+                    Log.LogInfo("Boss card removed from original deck.");
+                    break;
+                }
+            }
+        }
+
+        private bool ShouldRemoveBossCardFromDeck() => bossShrineCardSelectedCount == maxBossShrineCount.Value;
+        private bool ShouldSpawnBossShrine() => bossShrineCardSelectedCount < minBossShrineCount.Value;
+
+        private bool CanAffordBossShrine(int maxCost) => bossDeck.GetChoice(0).value.cost < maxCost;
+
+        private bool IsValidStage() => validStages.Contains(currentScene.sceneDef.baseSceneName);
+    }
+}
---
+++ MountainShrineConfig/Prefabs.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using System.Text;
+using UnityEngine;
+
+namespace MountainShrineConfig
+{
+    static class Prefabs
+    {
+		public static List<Type> types;
+
+		public static void LogPrefabs() 
+        {
+			string outDirPath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) + "/Bodys/";
+			string text2 = "";
+			Log.LogInfo(outDirPath);
+			text2 = text2 + outDirPath+ "\n";
+			types = new List<Type>();
+
+			var d = ScriptableObject.CreateInstance<RoR2.DirectorCardCategorySelection>();
+			foreach (var c in RoR2.ClassicStageInfo.instance.GetComponents<Component>())
+            {
+				Log.LogInfo($"Stage Component: {c.name}");
+            }
+			return;
+			try
+			{
+				text2 += "create Directory\n";
+				Directory.CreateDirectory(outDirPath);
+				text2 += "for each\n";
+				string text3;
+				foreach (GameObject gameObject in Resources.LoadAll<GameObject>("Prefabs/"))
+				{
+					Component[] components = gameObject.GetComponents<Component>();
+					text3 = "";
+					text3 = text3 + gameObject.name + "\n";
+					text3 += OutputComponents(components, ">");
+					text3 += GetChildren(gameObject.transform, ">");
+					File.WriteAllText(outDirPath + gameObject.name + ".txt", text3);
+				}
+				text2 += "types\n";
+				text3 = "";
+				text3 += "\n\ntypes\n\n";
+				for (int j = 0; j < types.Count; j++)
+				{
+					MemberInfo[] members = types[j].GetMembers();
+					text3 = text3 + types[j].Name + "\n";
+					for (int k = 0; k < members.Length; k++)
+					{
+						bool flag = !(members[k].DeclaringType.Name == "MonoBehaviour") && !(members[k].DeclaringType.Name == "Component") && !(members[k].DeclaringType.Name == "Behaviour") && !(members[k].DeclaringType.Name == "Object");
+						if (flag)
+						{
+							text3 = string.Concat(new string[]
+							{
+								text3,
+								"> ",
+								members[k].MemberType.ToString(),
+								": ",
+								members[k].Name,
+								"\n"
+							});
+						}
+					}
+				}
+				File.WriteAllText(outDirPath + "types.txt", text3);
+			}
+			catch (Exception ex)
+			{
+				text2 += ex.ToString();
+			}
+			File.WriteAllText(outDirPath + "log.txt", text2);
+		}
+
+		public static string OutputComponents(Component[] components, string delimi)
+		{
+			string text = "";
+			for (int i = 0; i < components.Length; i++)
+			{
+				string fullName = components[i].GetType().FullName;
+				string text2;
+				if (!(fullName == "UnityEngine.Transform"))
+				{
+					Type type = components[i].GetType();
+					text2 = type.FullName + "\n";
+					foreach (FieldInfo fieldInfo in type.GetFields())
+					{
+						text2 = string.Concat(new object[]
+						{
+							text2,
+							delimi,
+							"v ",
+							fieldInfo.Name,
+							" = ",
+							fieldInfo.GetValue(components[i]),
+							"\n"
+						});
+					}
+				}
+				else
+				{
+					Transform transform = (Transform)components[i];
+					text2 = string.Concat(new string[]
+					{
+						"transform = p:",
+						transform.localPosition.ToString(),
+						" r:",
+						transform.eulerAngles.ToString(),
+						" s:",
+						transform.localScale.ToString(),
+						"\n"
+					});
+				}
+				text = string.Concat(new string[]
+				{
+					text,
+					"\n",
+					delimi,
+					" ",
+					text2
+				});
+				bool flag = types.Contains(components[i].GetType());
+				if (flag)
+				{
+					types.Add(components[i].GetType());
+				}
+			}
+			return text;
+		}
+
+		// Token: 0x06000004 RID: 4 RVA: 0x000024D4 File Offset: 0x000006D4
+		public static string GetChildren(Transform transform, string delimi)
+		{
+			string text = "";
+			for (int i = 0; i < transform.childCount; i++)
+			{
+				GameObject gameObject = transform.GetChild(i).gameObject;
+				text = string.Concat(new string[]
+				{
+					text,
+					delimi,
+					"c ",
+					gameObject.name,
+					"\n"
+				});
+				Component[] components = gameObject.GetComponents<Component>();
+				text += OutputComponents(components, delimi + ">");
+				text += GetChildren(transform.GetChild(i), delimi + ">");
+			}
+			return text;
+		}
+	}
+}
